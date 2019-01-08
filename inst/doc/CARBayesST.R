c7b3db5cc9cd240a82b84a42f8bd4b5540f447ec
Duncan Lee <Duncan.Lee@glasgow.ac.uk>
2019-01-08 10:50:02 +0000

    version 3.0.1

--- inst/doc/CARBayesST.R
+++ inst/doc/CARBayesST.R
@@ -1,13 +1,13 @@
 ### R code from vignette source 'CARBayesST.Rnw'
 
 ###################################################
-### code chunk number 1: CARBayesST.Rnw:114-115
+### code chunk number 1: CARBayesST.Rnw:116-117
 ###################################################
 options(prompt = "R>  ")
 
 
 ###################################################
-### code chunk number 2: CARBayesST.Rnw:442-449
+### code chunk number 2: CARBayesST.Rnw:444-451
 ###################################################
 n.space <- 20
 N <- 20
@@ -19,7 +19,7 @@ N.all <- N * K
 
 
 ###################################################
-### code chunk number 3: CARBayesST.Rnw:454-457
+### code chunk number 3: CARBayesST.Rnw:456-459
 ###################################################
 distance <- as.matrix(dist(Grid))
 W <- array(0, c(K,K))
@@ -27,7 +27,7 @@ W[distance==1] <- 1
 
 
 ###################################################
-### code chunk number 4: CARBayesST.Rnw:464-467
+### code chunk number 4: CARBayesST.Rnw:466-469
 ###################################################
 distance <- as.matrix(dist(1:N))
 D <-array(0, c(N,N))
@@ -35,13 +35,13 @@ D[distance==1] <-1
 
 
 ###################################################
-### code chunk number 5: CARBayesST.Rnw:472-473
+### code chunk number 5: CARBayesST.Rnw:474-475
 ###################################################
 Q.W <- 0.8 * (diag(apply(W, 2, sum)) - W) + 0.2 * diag(rep(1,K))
 
 
 ###################################################
-### code chunk number 6: CARBayesST.Rnw:478-483
+### code chunk number 6: CARBayesST.Rnw:480-485
 ###################################################
 Q.W.inv <- solve(Q.W)
 library("MASS")
@@ -51,7 +51,7 @@ phi.long <- rep(phi, N)
 
 
 ###################################################
-### code chunk number 7: CARBayesST.Rnw:489-494
+### code chunk number 7: CARBayesST.Rnw:491-496
 ###################################################
 Q.D <- 0.8 * (diag(apply(D, 2, sum)) - D) + 0.2 * diag(rep(1, N))
 Q.D.inv <- solve(Q.D)
@@ -61,14 +61,14 @@ delta.long <- kronecker(delta, rep(1, K))
 
 
 ###################################################
-### code chunk number 8: CARBayesST.Rnw:499-501
+### code chunk number 8: CARBayesST.Rnw:501-503
 ###################################################
 x <- rnorm(n = N.all, mean = 0, sd = 1)
 gamma <- rnorm(n = N.all, mean = 0, sd = sqrt(0.01))
 
 
 ###################################################
-### code chunk number 9: CARBayesST.Rnw:506-512
+### code chunk number 9: CARBayesST.Rnw:508-514
 ###################################################
 beta1 <- 0
 beta2 <- 0.1
@@ -79,7 +79,7 @@ Y <- rbinom(n = N.all, size = n, prob = theta.true)
 
 
 ###################################################
-### code chunk number 10: CARBayesST.Rnw:650-654
+### code chunk number 10: CARBayesST.Rnw:652-656
 ###################################################
 library("CARBayesdata")
 library("sp")
@@ -88,13 +88,13 @@ data("pollutionhealthdata")
 
 
 ###################################################
-### code chunk number 11: CARBayesST.Rnw:660-661
+### code chunk number 11: CARBayesST.Rnw:662-663
 ###################################################
 head(pollutionhealthdata)
 
 
 ###################################################
-### code chunk number 12: CARBayesST.Rnw:667-674
+### code chunk number 12: CARBayesST.Rnw:669-676
 ###################################################
 pollutionhealthdata$SMR <- pollutionhealthdata$observed / 
     pollutionhealthdata$expected
@@ -106,7 +106,7 @@ pairs(pollutionhealthdata[ ,c(9, 5:7)], pch=19, cex=0.5, lower.panel=NULL,
 
 
 ###################################################
-### code chunk number 13: CARBayesST.Rnw:689-693
+### code chunk number 13: CARBayesST.Rnw:691-695
 ###################################################
 library("dplyr")
 SMR.av <- summarise(group_by(pollutionhealthdata,IG), SMR.mean = 
@@ -115,14 +115,14 @@ GGHB.IG@data$SMR <- SMR.av$SMR.mean
 
 
 ###################################################
-### code chunk number 14: CARBayesST.Rnw:700-702
+### code chunk number 14: CARBayesST.Rnw:702-704
 ###################################################
 library(rgdal)
 GGHB.IG <- spTransform(GGHB.IG, CRS("+proj=longlat +datum=WGS84 +no_defs"))
 
 
 ###################################################
-### code chunk number 15: CARBayesST.Rnw:707-717
+### code chunk number 15: CARBayesST.Rnw:709-719
 ###################################################
 library(leaflet)
 colours <- colorNumeric(palette = "BuPu", domain = GGHB.IG@data$SMR)
@@ -137,7 +137,7 @@ map1
 
 
 ###################################################
-### code chunk number 16: CARBayesST.Rnw:732-736
+### code chunk number 16: CARBayesST.Rnw:734-738
 ###################################################
 library("spdep")
 W.nb <- poly2nb(GGHB.IG, row.names = SMR.av$IG)
@@ -146,7 +146,7 @@ W <- nb2mat(W.nb, style = "B")
 
 
 ###################################################
-### code chunk number 17: CARBayesST.Rnw:746-752
+### code chunk number 17: CARBayesST.Rnw:748-754
 ###################################################
 formula <- observed ~ offset(log(expected)) + jsa + price + pm10
 model1 <- glm(formula = formula, family = "quasipoisson", 
@@ -157,13 +157,13 @@ summary(model1)$dispersion
 
 
 ###################################################
-### code chunk number 18: CARBayesST.Rnw:757-758
+### code chunk number 18: CARBayesST.Rnw:759-760
 ###################################################
 moran.mc(x = resid.glm[1:271], listw = W.list, nsim = 10000)
 
 
 ###################################################
-### code chunk number 19: CARBayesST.Rnw:883-887
+### code chunk number 19: CARBayesST.Rnw:885-889
 ###################################################
 library("CARBayesdata")
 library("sp")
@@ -172,7 +172,7 @@ data("salesdata")
 
 
 ###################################################
-### code chunk number 20: CARBayesST.Rnw:893-897
+### code chunk number 20: CARBayesST.Rnw:895-899
 ###################################################
 salesdata$salesprop <- salesdata$sales / salesdata$stock
 boxplot(salesdata$salesprop ~ salesdata$year, range = 0, xlab = "Year", 
@@ -181,7 +181,7 @@ boxplot(salesdata$salesprop ~ salesdata$year, range = 0, xlab = "Year",
 
 
 ###################################################
-### code chunk number 21: CARBayesST.Rnw:910-914
+### code chunk number 21: CARBayesST.Rnw:912-916
 ###################################################
 library("dplyr")
 salesprop.av <- summarise(group_by(salesdata, IG), 
@@ -190,7 +190,7 @@ GGHB.IG@data$sales <- salesprop.av$salesprop.mean
 
 
 ###################################################
-### code chunk number 22: CARBayesST.Rnw:919-932
+### code chunk number 22: CARBayesST.Rnw:921-934
 ###################################################
 library(rgdal)
 GGHB.IG <- spTransform(GGHB.IG, CRS("+proj=longlat +datum=WGS84 +no_defs"))
@@ -208,7 +208,7 @@ map1
 
 
 ###################################################
-### code chunk number 23: CARBayesST.Rnw:950-953
+### code chunk number 23: CARBayesST.Rnw:952-955
 ###################################################
 library("spdep")
 W.nb <- poly2nb(GGHB.IG, row.names = salesprop.av$salesprop.mean)
